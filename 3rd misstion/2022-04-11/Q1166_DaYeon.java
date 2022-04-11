377b9d74d8d7cbf5f6a7130b28565d8b42c4186f
dapri <102639884+da-pri@users.noreply.github.com>
2022-04-11 23:32:17 +0900

    Add files via upload

---
+++ 3rd misstion/2022-04-11/Q1166_DaYeon.java
@@ -0,0 +1,23 @@
+package CodeUp;
+
+import java.util.*;
+
+public class Q1166_DaYeon {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		
+		if(a <= 100) {
+			System.out.println(a+"%");
+		}
+	}
+
+}
+
+/*
+문제 이해도 : 5 (1 ~ 5 사이 숫자)
+문제 해결 : O
+코딩 시간 : 2분
+*/
