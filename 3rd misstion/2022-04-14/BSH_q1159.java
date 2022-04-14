9b30077fee7ed4337e1af35ba8d8e27ed62ce7ba
BarcordKiller <103096598+BarcordKiller@users.noreply.github.com>
2022-04-14 15:58:16 +0900

    Add files via upload

---
+++ 3rd misstion/2022-04-14/BSH_q1159.java
@@ -0,0 +1,21 @@
+package CodeUp;
+import java.util.Scanner;
+public class BSH_q1159 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int lc = sc.nextInt();
+		
+		if((lc>=50 && lc<=70) || lc%6==0) {
+			System.out.println("win");
+		}else
+			System.out.println("lose");
+		/*
+		 20220414
+		 문제이해도:4(1~5사이 숫자)
+		 문제 해결 : o
+		 코딩 시간 : 5분
+		 */
+	}
+
+}
---
+++ 3rd misstion/2022-04-14/BSH_q1160.java
@@ -0,0 +1,24 @@
+package CodeUp;
+import java.util.Scanner;
+public class BSH_q1160 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		
+		switch(num) {
+		case  1, 3, 5, 7 :
+			System.out.println("oh my god");
+			break;
+		default :
+			System.out.println("enjoy");
+		}
+
+	}
+	/*
+	 20220414
+	 문제이해도:4(1~5사이 숫자)
+	 문제 해결 : o
+	 코딩 시간 : 7분
+	 */
+}
