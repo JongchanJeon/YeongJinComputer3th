e2f3fc2c0158fb72411d159ac39135dfb59b4514
dapri <102639884+da-pri@users.noreply.github.com>
2022-04-14 15:55:18 +0900

    Add files via upload

---
+++ 3rd misstion/2022-04-14/YDY_Q_1156.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class YDY_Q_1156 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+	 int a = sc.nextInt();
+	 double b = sc.nextDouble();
+	 
+	 System.out.println((double)(a*b)/2);
+	}
+
+}
+/*
+2022-04-14
+문제 이해도 : 5
+문제 해결 : O
+코딩 시간 : 2분
+*/
