75d0a22062158c2a64f134ed7286979330cabe7a
Yehi-or <103096598+Yehi-or@users.noreply.github.com>
2022-04-27 17:41:38 +0900

    Add files via upload

---
+++ 3rd misstion/2022-04-27/BSH_Q1630.java
@@ -0,0 +1,24 @@
+package CodeUp;
+import java.util.Scanner;
+public class BSH_Q1630 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		String [] s = new String[n];
+		
+		for(int i=0; i<n; i++) {
+			String s2 = sc.next();
+			System.out.println(s2);
+			System.out.println("AMOLED");
+		}
+	}
+
+}
+/*
+20220427
+문제이해도:0(1~5사이 숫자)
+문제 해결 : x
+코딩 시간 :
+*/
