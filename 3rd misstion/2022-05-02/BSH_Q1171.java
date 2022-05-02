239af0a190e88143912d1ea5ebd25146eff8f60c
Yehi-or <103096598+Yehi-or@users.noreply.github.com>
2022-05-02 18:38:27 +0900

    Add files via upload

---
+++ 3rd misstion/2022-05-02/BSH_Q1171.java
@@ -0,0 +1,21 @@
+package CodeUp;
+import java.util.Scanner;
+public class BSH_Q1171 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt();
+		
+		System.out.print((a * 100000) + (b * 1000) + (c));
+
+	}
+
+}
+/*
+20220502
+문제이해도:4(1~5사이 숫자)
+문제 해결 : o
+코딩 시간 : 1분
+*/
