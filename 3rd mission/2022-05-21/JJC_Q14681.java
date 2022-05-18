5672bc12d2ca9b7f4baf65298ae5f3924a978006
JongchanJeon <tristan6349@naver.com>
2022-05-18 20:50:52 +0900

    Create JJC_Q14681.java

---
+++ 3rd mission/2022-05-21/JJC_Q14681.java
@@ -0,0 +1,30 @@
+package gitup;
+
+import java.util.Scanner;
+
+public class JJC_Q14681 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		
+		if (x > 0 && y > 0)
+			System.out.println("1");
+		else if(x < 0 && y > 0)
+			System.out.println("2");
+		else if(x > 0 && y < 0)
+			System.out.println("4");
+		else
+			System.out.println("3");
+
+	}
+
+}
+/*
+20220521
+문제 이해도 : 5 (1 ~ 5 사이 숫자)
+문제 해결 : O
+코딩 시간 : 2분
+*/
+
