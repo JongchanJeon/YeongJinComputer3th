f1814722b46312078b61a5ed810420c0ce0015df
dapri <102639884+da-pri@users.noreply.github.com>
2022-05-24 19:40:21 +0900

    Add files via upload

---
+++ 3rd mission/2022-05-24/YDY_Q2605.java
@@ -0,0 +1,28 @@
+package _2022_05_15;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class YDY_Q2605 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+	
+		int n = sc.nextInt();
+		ArrayList list = new ArrayList<>();
+		
+		for (int i = 0; i < n; i++) {
+			list.add(sc.nextInt(),i+1);
+		}
+		for (int i = n-1; i >= 0; i--) {
+			System.out.print(list.get(i)+" ");
+		}
+		
+	}
+}
+/*
+2022-05-24
+문제 이해도 : 5
+문제 해결 : O
+코딩 시간 : 7분
+*/
